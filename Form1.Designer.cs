6194728d038ef49efa4fe428dab65d165b478534
alexf <alexf@ALEX-YOGA>
2025-05-26 13:24:36 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace YoutubeToMP3Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanelMain = new TableLayoutPanel();
+            labelInformation = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            buttonCancel = new Button();
+            tableLayoutPanelMain.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            tableLayoutPanelMain.ColumnCount = 1;
+            tableLayoutPanelMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanelMain.Controls.Add(tableLayoutPanel1, 0, 1);
+            tableLayoutPanelMain.Controls.Add(labelInformation, 0, 0);
+            tableLayoutPanelMain.Dock = DockStyle.Fill;
+            tableLayoutPanelMain.Location = new Point(0, 0);
+            tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            tableLayoutPanelMain.Padding = new Padding(10);
+            tableLayoutPanelMain.RowCount = 2;
+            tableLayoutPanelMain.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanelMain.RowStyles.Add(new RowStyle());
+            tableLayoutPanelMain.Size = new Size(271, 231);
+            tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // labelInformation
+            // 
+            labelInformation.AutoSize = true;
+            labelInformation.Dock = DockStyle.Fill;
+            labelInformation.Location = new Point(13, 10);
+            labelInformation.Name = "labelInformation";
+            labelInformation.Size = new Size(245, 176);
+            labelInformation.TabIndex = 0;
+            labelInformation.Text = "This setup program will install FisherBit LLC's YouTube to MP3 Converter onto your computer.\r\n\r\nClick \"Install\" to begin installation.";
+            labelInformation.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(button1, 1, 0);
+            tableLayoutPanel1.Controls.Add(buttonCancel, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(13, 189);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(245, 29);
+            tableLayoutPanel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Right;
+            button1.Location = new Point(167, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Install";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            buttonCancel.Dock = DockStyle.Left;
+            buttonCancel.Location = new Point(3, 3);
+            buttonCancel.Name = "buttonCancel";
+            buttonCancel.Size = new Size(75, 23);
+            buttonCancel.TabIndex = 1;
+            buttonCancel.Text = "Cancel";
+            buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(271, 231);
+            Controls.Add(tableLayoutPanelMain);
+            Name = "Form1";
+            Text = "YouTube to MP3 Installer";
+            tableLayoutPanelMain.ResumeLayout(false);
+            tableLayoutPanelMain.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanelMain;
+        private Label labelInformation;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button buttonCancel;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace YoutubeToMP3Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
